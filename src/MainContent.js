5c50064a2f2ed9823ba915be89b842a4f747bc9d
HellForce <hellforce@inbox.ru>
2020-06-07 16:47:39 +0300

    Это наш первый коммит

---
+++ src/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function MainContent() {
+    return (
+<main>
+<h1>SIMONTSEV</h1>
+<h2>ANDREY</h2>
+<h3>ALEKSANDROVICH</h3>
+</main>
+    )
+}
+export default MainContent
---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+<nav styles='display: fix'>
+<a href='*'>Main</a> 
+<a href='./MainContent.js'>MainContent</a>
+<a href='Footer'>Footer</a>
+
+</nav>
+
+
+
+    )
+}
+export default NavBar
